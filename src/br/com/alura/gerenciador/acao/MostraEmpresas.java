6f656cfc077c657f0d4c81d386d86fecf2762e73
alansouza19 <alan.souza-97@hotmail.com>
2022-01-08 00:11:02 -0300

    Centralizando as ações no controlador

--- src/br/com/alura/gerenciador/acao/MostraEmpresas.java
+++ src/br/com/alura/gerenciador/acao/MostraEmpresas.java
@@ -2,7 +2,6 @@ package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,7 @@ import br.com.alura.gerenciador.modelo.Empresa;
 
 public class MostraEmpresas {
 
-	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		System.out.println("Mostrando dados da empresa");
 		
 		String paramId = request.getParameter("id");
@@ -25,7 +24,7 @@ public class MostraEmpresas {
 		System.out.println(empresa.getNome());
 		
 		request.setAttribute("empresa", empresa);
-		RequestDispatcher rd = request.getRequestDispatcher("/formAlteraEmpresa.jsp");
-		rd.forward(request, response);
+		
+		 return "forward:/formAlteraEmpresa.jsp";
 	}
 }
--- src/br/com/alura/gerenciador/acao/RemoveEmpresas.java
+++ src/br/com/alura/gerenciador/acao/RemoveEmpresas.java
@@ -10,7 +10,7 @@ import br.com.alura.gerenciador.modelo.Banco;
 
 public class RemoveEmpresas {
 
-	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		System.out.println("Removendo empresa");
 		
 		String paramId = request.getParameter("id");
@@ -21,6 +21,6 @@ public class RemoveEmpresas {
 		Banco banco = new Banco();
 		banco.removeEmpresa(id);
 		
-		response.sendRedirect("entrada?acao=ListaEmpresas");
+		return "redirect:entrada?acao=ListaEmpresas";
 	}
 }
